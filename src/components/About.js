200e9f883749c715cb70344fe39c8107773749e9
Maksym Trachenko <maks.trachenko@gmail.com>
2021-10-06 19:21:18 +0300

    about page finished, footer added

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const About = () => {
-  return <h2>About Garage!</h2>;
+  return (
+    <section className='about-section'>
+      <div className='about-content'>
+        <h1 id='about-title'>Who we are?</h1>
+        <p>
+          We are company which salse a street style clothing Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Nulla, iure.
+        </p>
+        <button className='hero-btn about'>Contact us</button>
+      </div>
+      <img
+        src='https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2xvdGhpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+        alt=''
+      />
+    </section>
+  );
 };
 
 export default About;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,11 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <footer className='footer'>
+      <ul className='footer-links'>
+        <li className='footer-icons'>instagram</li>
+        <li className='footer-icons'>telegram</li>
+        <li className='footer-icons'>github</li>
+      </ul>
       <p>
         &copy; <span id='date'></span>Garage, all rights reserved
       </p>
