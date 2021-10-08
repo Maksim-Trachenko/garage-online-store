d3e12d2e7fa0c3364b2d34d8f5d1b94071d3790e
Maksym Trachenko <maks.trachenko@gmail.com>
2021-10-08 17:00:29 +0300

    ClothingList component added

--- src/components/ForMen.js
+++ src/components/ForMen.js
@@ -1,74 +1,8 @@
 import React from 'react';
+import ClothingList from '../items/ClothingList';
 import { Link } from 'react-router-dom';
 
 const ForMen = () => {
-  return (
-    <section className='forMen-section'>
-      <aside className='categories-section'>
-        <ul className='category'>
-          <li>T-shirts</li>
-          <li>Hoodies</li>
-          <li>Trousers</li>
-          <li>Shoes</li>
-        </ul>
-      </aside>
-      <article className='forMen-clothing-section'>
-        <div className='forMen-title'>
-          <h1>For Men</h1>
-          <p>T-shirts</p>
-        </div>
-        <main className='forMen-catalogue'>
-          <div className='forMen-element'>
-            <div className='forMen-element-image'>
-              <img
-                src='https://static.super-shop.com/1253609-the-north-face-black-box-search-rescue-tshirt-tnf-black-960w.jpg'
-                alt=''
-              />
-            </div>
-            <div className='forMen-element-text'>
-              <p>basic TNF t-shirt</p>
-              <h3>45$</h3>
-            </div>
-          </div>
-          <div className='forMen-element'>
-            <div className='forMen-element-image'>
-              <img
-                src='https://static.super-shop.com/1253609-the-north-face-black-box-search-rescue-tshirt-tnf-black-960w.jpg'
-                alt=''
-              />
-            </div>
-            <div className='forMen-element-text'>
-              <p>basic TNF t-shirt</p>
-              <h3>45$</h3>
-            </div>
-          </div>
-          <div className='forMen-element'>
-            <div className='forMen-element-image'>
-              <img
-                src='https://static.super-shop.com/1253609-the-north-face-black-box-search-rescue-tshirt-tnf-black-960w.jpg'
-                alt=''
-              />
-            </div>
-            <div className='forMen-element-text'>
-              <p>basic TNF t-shirt</p>
-              <h3>45$</h3>
-            </div>
-          </div>
-          <div className='forMen-element'>
-            <div className='forMen-element-image'>
-              <img
-                src='https://static.super-shop.com/1253609-the-north-face-black-box-search-rescue-tshirt-tnf-black-960w.jpg'
-                alt=''
-              />
-            </div>
-            <div className='forMen-element-text'>
-              <p>basic TNF t-shirt</p>
-              <h3>45$</h3>
-            </div>
-          </div>
-        </main>
-      </article>
-    </section>
-  );
+  return <ClothingList />;
 };
 export default ForMen;
---
+++ src/items/ClothingList.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ClothingList = () => {
+  return (
+    <section className='forMen-section'>
+      <aside className='categories-section'>
+        <ul className='category'>
+          <li>T-shirts</li>
+          <li>Hoodies</li>
+          <li>Trousers</li>
+          <li>Shoes</li>
+        </ul>
+      </aside>
+      <article className='forMen-clothing-section'>
+        <div className='forMen-title'>
+          <h1>For Men</h1>
+          <p>T-shirts</p>
+        </div>
+        <main className='forMen-catalogue'>
+          <div className='forMen-element'>
+            <div className='forMen-element-image'>
+              <img
+                src='https://static.super-shop.com/1253609-the-north-face-black-box-search-rescue-tshirt-tnf-black-960w.jpg'
+                alt=''
+              />
+            </div>
+            <div className='forMen-element-text'>
+              <p>basic TNF t-shirt</p>
+              <h3>45$</h3>
+            </div>
+          </div>
+          <div className='forMen-element'>
+            <div className='forMen-element-image'>
+              <img
+                src='https://static.super-shop.com/1253609-the-north-face-black-box-search-rescue-tshirt-tnf-black-960w.jpg'
+                alt=''
+              />
+            </div>
+            <div className='forMen-element-text'>
+              <p>basic TNF t-shirt</p>
+              <h3>45$</h3>
+            </div>
+          </div>
+          <div className='forMen-element'>
+            <div className='forMen-element-image'>
+              <img
+                src='https://static.super-shop.com/1253609-the-north-face-black-box-search-rescue-tshirt-tnf-black-960w.jpg'
+                alt=''
+              />
+            </div>
+            <div className='forMen-element-text'>
+              <p>basic TNF t-shirt</p>
+              <h3>45$</h3>
+            </div>
+          </div>
+          <div className='forMen-element'>
+            <div className='forMen-element-image'>
+              <img
+                src='https://static.super-shop.com/1253609-the-north-face-black-box-search-rescue-tshirt-tnf-black-960w.jpg'
+                alt=''
+              />
+            </div>
+            <div className='forMen-element-text'>
+              <p>basic TNF t-shirt</p>
+              <h3>45$</h3>
+            </div>
+          </div>
+        </main>
+      </article>
+    </section>
+  );
+};
+
+export default ClothingList;
