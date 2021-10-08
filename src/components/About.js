fbf23bef77f124173613c2ab6fa48577b9b2f0fe
Maksym Trachenko <maks.trachenko@gmail.com>
2021-10-08 13:15:37 +0300

    contact page created

--- src/components/About.js
+++ src/components/About.js
@@ -7,10 +7,12 @@ const About = () => {
       <div className='about-content'>
         <h1 id='about-title'>Who we are?</h1>
         <p>
-          We are company which salse a street style clothing Lorem ipsum dolor
+          We are company which sales a street style clothing Lorem ipsum dolor
           sit amet consectetur adipisicing elit. Nulla, iure.
         </p>
-        <button className='hero-btn about'>Contact us</button>
+        <Link to='/contacts'>
+          <button className='hero-btn about'>Contact us</button>
+        </Link>
       </div>
       <img
         src='https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2xvdGhpbmd8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,7 +2,70 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Contacts = () => {
-  return <h2>Contacts Garage!</h2>;
+  return (
+    <section className='contact-container'>
+      <div className='contact-text'>
+        <h2>Contact us</h2>
+        <p>If you have any questions, text us!</p>
+      </div>
+      <div className='form-page'>
+        <form action=''>
+          <div className='form-group'>
+            <label htmlFor='fname' classname='form-label'>
+              First Name
+            </label>
+            <input
+              type='text'
+              className='form-input'
+              id='fname'
+              name='firstName'
+              placeholder='Brad'
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='lname' classname='form-label'>
+              Last Name
+            </label>
+            <input
+              type='text'
+              className='form-input'
+              id='lname'
+              name='lastName'
+              placeholder='Pitt'
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='email' classname='form-label'>
+              Your email
+            </label>
+            <input
+              type='email'
+              className='form-input'
+              id='email'
+              name='emailform'
+              placeholder='brad.pitt@example.com'
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='message' classname='form-label'>
+              Message
+            </label>
+            <textarea
+              name='messageForm'
+              id='message'
+              cols='35'
+              rows='7'
+              className='form-input'
+              placeholder='Enter you message...'
+            ></textarea>
+          </div>
+          <div className='submit-btn'>
+            <input type='submit' className='form-btn' value='Submit' />
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contacts;
